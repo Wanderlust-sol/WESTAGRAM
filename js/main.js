3d586f146d7c572f9e1dad4af53397dddff0c539
Taesol Kwon <ds3bbbb@naver.com>
2020-02-04 16:41:37 +0900

    delete Comment Function
    New Function!!

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const innerForm = document.querySelector(".content_part");
 const commentInput = document.querySelector(".addcomment_box");
 const postBtn = document.querySelector(".post_button");
+const thisIsTextarea = document.querySelector("textarea");
+const parentForm = document.querySelector(".parent_form");
 
 function uploadComment() {
   const commentValue = commentInput.value;
@@ -10,7 +12,7 @@ function uploadComment() {
   const createTag3 = document.createElement("span");
   const imgTag = document.createElement("img");
 
-  if (commentValue) {
+  if (commentValue !== "") {
     createTag.className += "newDiv";
     createTag1.className += "name";
     createTag2.className += "newComment";
@@ -21,16 +23,28 @@ function uploadComment() {
     createTag1.innerHTML = "wanderlust_sol";
     createTag2.innerHTML = commentValue;
 
-    innerForm.appendChild(createTag);
+    parentForm.appendChild(createTag);
     createTag.appendChild(createTag1);
     createTag.appendChild(createTag2);
     createTag.appendChild(createTag3);
     createTag3.appendChild(imgTag);
+    thisIsTextarea.value = "";
+  }
+
+  createTag.addEventListener("click", function(e) {
+    e.target.remove();
+  });
+}
+
+function enterKeycode(e) {
+  if (e.keyCode === 13) {
+    uploadComment();
   }
 }
 
 function pressBtn() {
   postBtn.addEventListener("click", uploadComment);
+  commentInput.addEventListener("keydown", enterKeycode);
 }
 
 pressBtn();
